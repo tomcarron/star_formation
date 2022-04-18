fae24987bddf083b53d2c55fc2f07e0a6b200d17
tomcarron <tcarron@tcd.ie>
2022-04-18 23:01:49 +0200

    removed debugging couts and added some good commenting

--- ex2/ex2.cpp
+++ ex2/ex2.cpp
@@ -9,13 +9,14 @@ using std::pow;
 using std::setw;
 using std::vector;
 using namespace std;
+using std::string;
+using std::setprecision;
 
 /* Code for exercise 2 of tcsf, basic functions for Smoothed Particles Hydrodynamics (SPH) */
 
-int n=100;  //number of particles in system
-//float n2=n;
-float mass=1.0; //mass of all the particles is the same and set to 1.
-float eta=8;  //eta for smoothing length. Is of order unity and between 2 and 10.
+int n=100;                          //number of particles in system
+float mass=1.0;                     //mass of all the particles is the same and set to 1.
+vector<float> etas={2,4,6,8,10};    //eta for smoothing length. Is of order unity and between 2 and 10.
 
 //Function to print the values of a vector of floats
 void printVec(vector<float> vec){
@@ -30,7 +31,7 @@ void printVecInt(vector<int> vec){
     }
 }
 // Function to take a vector of floats and save to a txt file named "name.txt" for plotting in python.
-void output(vector<float> vec, char name[]){
+void output(vector<float> vec, string name){
     fstream file;
     file.open(name,ios_base::out);
 
@@ -45,14 +46,10 @@ void output(vector<float> vec, char name[]){
 }
 //Now write a function to edit the values of the vector to be evenly spaced between 0 and n but not exactly at n.
 vector<float> setPositions(vector<float> vec){
-    //cout << "vecsize= " << vec.size() <<"\n";
     for ( int i = 0; i < vec.size(); i++ ) {
         float i2=i;
         vec.at(i) = ((i2 / (vec.size()))+(1.0/(vec.size()+1))); // set element at location i to i /n
     }
-   // vec.pop_back();  //removes back value
-    //vec.pop_back();  //removes back value
-    //printVec(vec);
     return vec;
 }
 //Function to calculate the smoothing length
@@ -62,12 +59,8 @@ float SmoothLength(float eta,vector<float> vec){
         float dist=vec.at(i+1)-vec.at(i);   //distance between i and i+1 positions
         sum=sum+dist;                          //summing distances
     }
-    //cout << "sum -> " << sum <<"\n";
     float mean=sum/(vec.size()-1);            //mean of the distances, dividing sum of distances by number of distances
-    //cout << "mean -> " << mean <<"\n";
-    //cout << "n2 -> " << n2 <<"\n";
     float h=eta*(mean);
-    //cout << "h -> " << h <<"\n";
     return h;
 }
 
@@ -79,7 +72,6 @@ vector<int> neighbours(int pos, vector<float> vec, float h){
             newvec.push_back(i);
         }
     }
-    //printVec(newvec);
     return newvec;
 }
 //Function to evaluate the M4 spline kernel for a given smoothing length and neigbour distance
@@ -87,30 +79,18 @@ float M4kernel(float pos1,float pos2,float h){
     float norm=(2.0/(3.0*h));
     float s=(abs(pos1-pos2) / h);
     float w=0.0;
-    //cout << "pos1,pos2= " << pos1 << " " << pos2 << "\n";
-    //cout << "s= " << s << "\n";
-    //cout << "s^2= " << pow(s,2.0) << "\n";
-    //cout << "(2-s)^3= " << pow((2-s),3.0) << "\n";
     if (s>=0.0 && s<=1.0){
         w=(norm*(1.0-((3.0/2.0)*(pow(s,2.0)))+((3.0/4.0)*(pow(s,3.0)))));
-        //cout << "s= " << s << "\n";
-        //cout << "w= " << w << "\n";
     }
     else if (s>1.0 && s<=2.0){
         w=(norm*((1.0/4.0)*(pow((2-s),3.0))));
-        //cout << "s= " << s << "\n";
-        //cout << "w= " << w << "\n";
     }
     else if (s>2.0){
         w=0.0;
-        //cout << "s= " << s << "\n";
-        //cout << "w= " << w << "\n";
     }
     else {
         cout << "error in kernel evaluation bozo";
     }
-    //cout << "w= " << w << "\n";
-    //cout << "norm= " << norm << "\n";
     return w;
 }
 
@@ -119,49 +99,33 @@ float M4kernel(float pos1,float pos2,float h){
 //then we calculate the density at each position and return it as a vector.
 
 vector<float> density(float mass,float h, vector<float> vec){
-    vector<float> density;
-    for ( int i = 0; i < (vec.size()); i++ ){
-        vector<int> nn=neighbours(i,vec,h);     //creating a vector of the nearest neighbours of particle i.
-        //printVecInt(nn);
-        //cout << i;
+    vector<float> density;                                                       //intialising vector
+    for ( int i = 0; i < (vec.size()); i++ ){                                   //loop through each position
+        vector<int> nn=neighbours(i,vec,h);                                     //creating a vector of the nearest neighbours of particle i.
         float temp=0.0;
-        //cout << temp << "=temp\n";
-        for (int j =0; j < nn.size(); j++){
-                //cout << "i= " << i <<"j= " << j << "\n";
-                //cout << "i -> " << i << "j -> " << (nn.at(j)) << "\n";
-                float w = (mass*(M4kernel(vec.at(i),vec.at(nn.at(j)),h)));
-                //cout << "M4kernel= " << (M4kernel(vec.at(i),vec.at(nn.at(j)),h)) << "\n";
-                //cout << "vec.at(i)= " << vec.at(i) << " vec.at(nn.at(j))= " << vec.at(nn.at(j)) << "\n";
-                //cout << "w= " << w << "\n";
+        for (int j =0; j < nn.size(); j++){                                     //loop through each neighbour
+                float w = (mass*(M4kernel(vec.at(i),vec.at(nn.at(j)),h)));      //contribution of neighbour a j to density at i.
                 temp =temp + w;
-                //cout << temp << w << "temp, w\n";
         }
-        density.push_back(temp);
+        density.push_back(temp);                                               //adding density to end of vector
 
     }
-    return density;
+    return density;                                                             //returning vector of densities
 }
 
 
 int main () {
-    vector<float> positions(n,0);  //vector of size n, each element is 0.
-    //printVec(positions);
-    //cout << positions.size() << "positions size\n";
-    vector<float> newpos=setPositions(positions);
-    //cout << newpos.size() << "newpos size\n";
-    printVec(newpos);
-    float h=SmoothLength(eta,newpos);
-    cout <<"smoothing length -> " << h << "\n";
-    //vector<int> test=neighbours(2,newpos,h);
-    //printVecInt(test);
-    vector<float> densities=density(mass,h,newpos);
-    printVec(densities);
-    cout << densities.size() << " <- densities size\n";
-    //vector<int> nn=neighbours(12,newpos,h);
-    //printVecInt(nn);
-    char filename[]="densities.txt";
-    output(densities,filename);
-    char file2[]="positions.txt";
-    output(newpos,file2);
+    vector<float> positions(n,0);                               //vector of size n, each element is 0.
+    vector<float> newpos=setPositions(positions);               // set the positions to n evenly spaced values between 0 and 1, not including 0 and 1.
+    for (int i=0; i < etas.size(); i++){                        // loop through values of vector of eta values.
+        float eta=etas[i];                                      //current eta
+        string eta_string=to_string(llround(eta));              //eta as string for naming
+        float h=SmoothLength(eta,newpos);                       //calculate smoothing length
+        vector<float> densities=density(mass,h,newpos);         // compute densities at each position
+        string filename=eta_string+"_densities.txt";            //filename to save densities vector
+        output(densities,filename);                             //output densities to .txt
+        string file2=eta_string+"_positions.txt";               //filename to save positions vector
+        output(newpos,file2);                                   //output densities to .txt
 
+    }
 }
--- ex2/plot.py
+++ ex2/plot.py
@@ -5,22 +5,25 @@ import matplotlib.pyplot as plt
 """
 Script to import text files outputted by ex2.cpp and plot them
 """
-densities=[]
-file1=open("densities.txt","r")
-for line in file1.readlines():
-    densities.append(float(line))
+etas=[2,4,6,8,10]
+for i in range(len(etas)):
+    densities=[]
+    file1=open(str(etas[i])+"_densities.txt","r")
+    for line in file1.readlines():
+        densities.append(float(line))
 
-positions=[]
-file2=open("positions.txt","r")
-for line in file2.readlines():
-    positions.append(float(line))
+    positions=[]
+    file2=open(str(etas[i])+"_positions.txt","r")
+    for line in file2.readlines():
+        positions.append(float(line))
 
-#print(densities)
-#print(positions)
+    #print(densities)
+    #print(positions)
 
-fig, ax=plt.subplots(1)
-ax.scatter(positions,densities)
-ax.set_ylabel("Density")
-ax.set_xlabel("Position")
-plt.savefig("plots/densities.png",dpi=400,bbox_inches="tight")
-plt.show()
+    fig, ax=plt.subplots(1)
+    ax.scatter(positions,densities,label="$\eta$="+str(etas[i]))
+    ax.set_ylabel("Density")
+    ax.set_xlabel("Position")
+    ax.legend(loc="lower center")
+    plt.savefig("plots/densities_eta"+str(etas[i])+".png",dpi=400,bbox_inches="tight")
+    #plt.show()
